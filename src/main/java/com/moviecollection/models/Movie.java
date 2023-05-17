d85b2e153fffbabed2af2fc66af4b5e81a1a2f3a
anastasija <anastasija.man@gmail.com>
2023-05-17 20:09:54 +0300

    Movie model created

--- src/main/java/com/moviecollection/models/Movie.java
+++ src/main/java/com/moviecollection/models/Movie.java
@@ -1,4 +1,32 @@
 package com.moviecollection.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String title;
+    private Integer year;
+    private String genre;
+    private String description;
+    private Integer runtime;
+    private String trailerUrl;
+    private String posterUrl;
+    private Double rating;
+    private String actors;
+
 }
